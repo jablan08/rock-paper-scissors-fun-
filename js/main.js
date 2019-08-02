b685b4be3e40f3a82985e2ca1ec290ee51ae6b49
Jablan <joshuaablan@gmail.com>
2019-08-02 10:22:36 -0700

    set up render function

--- js/main.js
+++ js/main.js
@@ -3,25 +3,26 @@ const beepAudio = new Audio('http://soundbible.com/mp3/Robot_blip-Marianne_Gagno
 const shootAudio = new Audio('http://soundbible.com/mp3/shooting_star-Mike_Koenig-1132888100.mp3');
 
 /*----- app's state (variables) -----*/
-let scores, results;
+let scores, results, winner;
 
 /*----- cached element references -----*/
-
+const scoreEls = {
+    p: document.getElementById("p-score"),
+    t: document.getElementById("t-score"),
+    c: document.getElementById("c-score")
+};
 
 
 /*----- functions -----*/
 const playRound = () => {
-    console.log("hit")
-}
-
-const render = () => {
     
 }
+
 const init = () => {
-    score = {
-        p:0,
-        t:0,
-        c:0
+    scores = {
+        p: 0,
+        t: 0,
+        c: 0
     };
     results = {
         p: "r",
@@ -31,6 +32,11 @@ const init = () => {
     render();
 }
 
+const render = () => {
+    for (let score in scores) {
+        scoreEls[score]
+    }
+}
 init();
 
 /*----- event listeners -----*/
