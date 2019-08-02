1a3893782ec39d8ad1232362d277e4256a14b451
Jablan <joshuaablan@gmail.com>
2019-08-02 10:02:58 -0700

    make sure button is connection on event listener

--- js/main.js
+++ js/main.js
@@ -3,12 +3,36 @@ const beepAudio = new Audio('http://soundbible.com/mp3/Robot_blip-Marianne_Gagno
 const shootAudio = new Audio('http://soundbible.com/mp3/shooting_star-Mike_Koenig-1132888100.mp3');
 
 /*----- app's state (variables) -----*/
-
+let scores, results;
 
 /*----- cached element references -----*/
 
 
-/*----- event listeners -----*/
-
 
 /*----- functions -----*/
+const playRound = () => {
+    console.log("hit")
+}
+
+const render = () => {
+    
+}
+const init = () => {
+    score = {
+        p:0,
+        t:0,
+        c:0
+    };
+    results = {
+        p: "r",
+        c: "r"
+    };
+    winner = null;
+    render();
+}
+
+init();
+
+/*----- event listeners -----*/
+document.querySelector("main button")
+addEventListener("click", playRound)
